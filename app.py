16120e8fab79a69292cfc5cd61c920965c6fe06f
didattica-raf <didattica@scuolacinofila.net>
2025-09-08 12:33:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ st.markdown(
 )
 
 # Mostra il logo in cima alla pagina
-#st.image("logo.gif", width=139)
+st.image("logo.gif", width=139)
 
 # Streamlit UI setup
 st.set_page_config(page_title="Chat del Corso", page_icon="https://www.istitutoleopardi.it/wp-content/uploads/Leo-favicon-1.gif")
